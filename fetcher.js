84f41783532ddcdbc02ae530d1dca87c54e4ff80
Ramy Mohamed <94903106+theKidCelo@users.noreply.github.com>
2023-01-21 06:19:10 -0500

    Update fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -2,9 +2,7 @@ const args = process.argv.slice(2, 4);
 const fs = require('fs')
 const request = require('request');
 
-
 request(args[0], (error, response, body) => {
-
   console.log('statusCode:', response && response.statusCode);
   
   fs.writeFile(args[1], body, err => {
@@ -12,7 +10,7 @@ request(args[0], (error, response, body) => {
       console.error(error)
       return
     }
-    //file written successfully
+    
     console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
   })
 
